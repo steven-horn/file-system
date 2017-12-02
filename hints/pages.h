d0034367983dfd35248451aa837c78310c7308c4
Spencer LaChance <spencerlachance25@gmail.com>
2017-12-02 16:43:22 -0500

    made the bitmap

--- hints/pages.h
+++ hints/pages.h
@@ -3,12 +3,25 @@
 
 #include <stdio.h>
 
+#include "bitmap.h"
+
 typedef struct inode {
     int refs; // reference count
     int mode; // permission & type
     int size; // bytes for file
     int xtra; // more stuff can go here
 } inode;
+
+typedef struct super {
+    unsigned char *imap;
+    int imap_size;
+    unsigned char *dmap;
+    int dmap_size;
+    int num_inodes;
+    int num_blocks;
+    inode* inodes;
+    void* blocks;
+} super;
 
 void   pages_init(const char* path);
 void   pages_free();
